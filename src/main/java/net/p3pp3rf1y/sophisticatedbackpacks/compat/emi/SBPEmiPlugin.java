c90da0651983118450a8d14d8716729afcd91c20
Phoupraw <467934387@qq.com>
2024-10-30 19:54:52 +0800

    导入EMI

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/emi/SBPEmiPlugin.java
@@ -0,0 +1,13 @@
+package net.p3pp3rf1y.sophisticatedbackpacks.compat.emi;
+
+import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+
+@EmiEntrypoint
+public final class SBPEmiPlugin implements EmiPlugin {
+    @Override
+    public void register(EmiRegistry registry) {
+    
+    }
+}
